da1336b6a3741ee035eb75ac0862d0e8756766a4
Nakashima-0706 <k24109kk@aitech.ac.jp>
2025-10-23 15:36:41 +0900

    nakashima

--- saver.py
+++ saver.py
@@ -1,11 +1,9 @@
 
-# 保存
 import time
 
 def save_text(outputmoji: str) -> str:
     filename = time.strftime("%Y%m%d_%H%M%S") + "_transcript.txt"
     with open(filename, "w", encoding="utf-8") as f:
         f.write(outputmoji + "\n")
-    # ファイル名を返す
     return filename
 
